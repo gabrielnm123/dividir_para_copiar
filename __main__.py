4db408c501a4ae25b22601b94b38d36a80361a3c
gabrielnm123 <gabrielnmatos96@gmail.com>
2023-07-22 20:48:17 -0300

    first commit

---
+++ __main__.py
@@ -0,0 +1,33 @@
+from subprocess import run
+import pyperclip
+
+run('clear')
+linha_parada = input('linha de parada: ')
+linhas = []
+while True:
+    linha = input('texto: ')
+    if linha == linha_parada:
+        texto = '\n'.join(linhas)
+        break
+    linhas.append(linha)
+n_caracteres = len(texto)
+print(f'o texto tem {n_caracteres} caracteres')
+divicao_caracteres = int(input('numeros de caracteres: '))
+texto_dividido = list()
+tem_o_tamanho = True
+
+while True:
+    if n_caracteres > divicao_caracteres:
+        texto_dividido.append(texto[:divicao_caracteres])
+        texto = texto.replace(texto_dividido[-1], '')
+        n_caracteres_anterior = n_caracteres
+        n_caracteres = n_caracteres - divicao_caracteres
+        tem_o_tamanho = False
+    else:
+        if n_caracteres != - n_caracteres_anterior:
+            texto_dividido.append(texto[:divicao_caracteres])
+        break
+            
+
+def copy(indice:int) -> None:
+    pyperclip.copy(texto_dividido[indice])
